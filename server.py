2b71627968563b70f9d25a9359e7110d0f84850c
Lukas Rimkus <lukasrimkuslg@gmail.com>
2018-09-20 11:25:42 +0300

    Update server.py

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ A1_3 = bytes('A1|749376789GDLSL|         |\n', 'utf-8')
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
-BUFFER_SIZE = 1024  # Normally 1024, but we want fast response
+BUFFER_SIZE = 1024  
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
